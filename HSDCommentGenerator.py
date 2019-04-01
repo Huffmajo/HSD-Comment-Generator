a6e69a76bbf54fcbe969c90e50fb9244710a443d
Joel Huffman <huffmajo@oregonstate.edu>
2019-03-31 21:22:53 -0700

    Fixed copy to clipboard button. Updated one-off comments.

--- HSDCommentGenerator.py
+++ HSDCommentGenerator.py
@@ -11,6 +11,9 @@ varApplied = IntVar()
 varIFWI = IntVar()
 varEC = IntVar()
 varVideo = IntVar()
+varComment = IntVar()
+varOneOff = IntVar()
+varTicketType = IntVar()
 
 # type of comment prompt
 labelCommentType = Label(window, text="1. Choose comment type ", font=("Helvetica, 14"))
@@ -29,8 +32,16 @@ def selectRadio():
 		checkEc.config(state=NORMAL)
 		checkVideo.config(state=NORMAL)
 		entryVideo.config(state=NORMAL)
+		checkComment.config(state=NORMAL)
+		entryComment.config(state=NORMAL)
 		entryReworkTicketNumber.config(state=DISABLED)
-	
+		radioSLA.config(state=DISABLED)
+		radioWrongTicket.config(state=DISABLED)
+		radioTicketRework.config(state=DISABLED)
+		radioTicketDebug.config(state=DISABLED)
+		radioTicketInv.config(state=DISABLED)
+		radioTicketShipping.config(state=DISABLED)
+
 	# allow rework fields
 	elif var.get() == 2:
 		checkApplied.config(state=DISABLED)
@@ -39,8 +50,16 @@ def selectRadio():
 		checkEc.config(state=DISABLED)
 		checkVideo.config(state=DISABLED)
 		entryVideo.config(state=DISABLED)
+		checkComment.config(state=DISABLED)
+		entryComment.config(state=DISABLED)
 		entryReworkTicketNumber.config(state=NORMAL)
-	
+		radioSLA.config(state=DISABLED)
+		radioWrongTicket.config(state=DISABLED)
+		radioTicketRework.config(state=DISABLED)
+		radioTicketDebug.config(state=DISABLED)
+		radioTicketInv.config(state=DISABLED)
+		radioTicketShipping.config(state=DISABLED)	
+
 	# allow other fields
 	elif var.get() == 3:
 		checkApplied.config(state=DISABLED)
@@ -49,7 +68,15 @@ def selectRadio():
 		checkEc.config(state=DISABLED)
 		checkVideo.config(state=DISABLED)
 		entryVideo.config(state=DISABLED)
+		checkComment.config(state=DISABLED)
+		entryComment.config(state=DISABLED)
 		entryReworkTicketNumber.config(state=DISABLED)
+		radioSLA.config(state=NORMAL)
+		radioWrongTicket.config(state=NORMAL)
+		radioTicketRework.config(state=NORMAL)
+		radioTicketDebug.config(state=NORMAL)
+		radioTicketInv.config(state=NORMAL)
+		radioTicketShipping.config(state=NORMAL)
 	else:
 		print("Something went wrong")
 
@@ -70,8 +97,6 @@ def createComment():
 				comment += "Reworks %s applied\n" % reworks
 			else:
 				comment += "Required reworks applied\n"
-		else:
-			comment += "Up-to-date on required reworks\n"
 
 		# add IFWI and EC messages based on checks
 		if varIFWI.get() == 1:
@@ -91,6 +116,11 @@ def createComment():
 			else:
 				comment += "\n"
 
+		# add additional comment
+		if varComment.get() == 1:
+			if entryComment.get() != "":
+				comment += "%s\n"% entryComment.get()
+
 		# ending text
 		comment += "Records updated in ISMP\nReady for pickup at HF1 inventroy cage\n"
 	
@@ -105,8 +135,34 @@ def createComment():
 
 	# create other comment
 	elif var.get() == 3:
-		comment = "Other comments go here\n"
-	
+
+		# SLA time info
+		if varOneOff.get() == 1:
+		
+			comment = "Current SLA times for tickets through HSD-ES are as follows:\nLOW - 10 business days\nMEDIUM - 7 business days\nHIGH - 5 business days\nSHOWSTOPPER - 2 business days with valid showstopper reasoning\n\nIf you have any questions or concerns about SLA times, don't hesistate to reach out to lab manager Charles \"Chuck\" Kelley.\nThanks."
+
+		# Wrong ticket type
+		elif varOneOff.get() == 2:
+
+			# correct ticket type
+			if varTicketType.get() == 1:
+				type = "lab.rework"
+			elif varTicketType.get() == 2:
+				type = "lab.debug"
+			elif varTicketType.get() == 3:
+				type = "lab.inventory"
+			elif varTicketType.get() == 4:
+				type = "lab.shipping"
+			else:
+				type = "NULL"
+
+			if type == "NULL":
+				comment = "Choose a ticket type to correct to.\n"
+			else:
+				comment = "Ticket type changed to %s. The lab.build component type is reserved for building up new boards or updating hardware. This includes installing standoffs, installing CPU and PCH mounting hardware, installing memory, seating silicon, flashing IFWI and EC and boot-testing the board. Thanks.\n" % type
+		else:
+			comment = ""
+
 	# no comment type was selected
 	else:
 		comment = "No comment type selected\n"
@@ -115,9 +171,9 @@ def createComment():
 	generatedText.insert(INSERT, comment)
 
 def copyToClipboard():
-	copyText = generatedText.get()
+	copyText = generatedText.get("1.0", END)
 	window.clipboard_clear()
-	window.clipboard_append("copyText")
+	window.clipboard_append(copyText)
 
 # type of comment choices
 radioComplete = Radiobutton(window, text="Completed", variable=var, value=1, command=selectRadio)
@@ -125,7 +181,7 @@ radioReworks = Radiobutton(window, text="Sent for reworks", variable=var, value=
 radioOther = Radiobutton(window, text="Other", variable=var, value=3, command=selectRadio)
 radioComplete.grid(column=0, row=1, sticky="w")
 radioReworks.grid(column=0, row=6, sticky="w")
-radioOther.grid(column=0, row=8, sticky="w")
+radioOther.grid(column=0, row=7, sticky="w")
 
 # COMPLETE CHOICES
 # reworks applied
@@ -150,6 +206,13 @@ checkVideo.grid(column=1, row=4, sticky="w")
 entryVideo = Entry(window, width=40)
 entryVideo.grid(column=2, row=4, sticky="w")
 
+# misc comments
+checkComment = Checkbutton(window, text="Additional comments", variable=varComment)
+checkComment.grid(column=1, row=5, sticky="w")
+
+entryComment = Entry(window, width=40)
+entryComment.grid(column=2, row=5, sticky="w")
+
 # REWORK CHOICES
 # ticket number
 labelReworkTicketNumber = Label(window, text="Rework ticket number")
@@ -158,23 +221,36 @@ labelReworkTicketNumber.grid(column=1, row=6, sticky="w")
 entryReworkTicketNumber = Entry(window, width=40)
 entryReworkTicketNumber.grid(column=2, row=6, sticky="w")
 
-
 # OTHER choices
-# first one-off
-labelOther = Label(window, text="One-offs will go here")
-labelOther.grid(columnspan=2, column=1, row=8)
+# One-off radio buttons
+radioSLA = Radiobutton(window, text="SLA times", variable=varOneOff, value=1)
+radioWrongTicket = Radiobutton(window, text="Wrong ticket type", variable=varOneOff, value=2)
+radioSLA.grid(column=1, row=7, sticky="w")
+radioWrongTicket.grid(column=1, row=8, sticky="w")
+
+# ticket type corrections
+radioTicketRework = Radiobutton(window, text="Rework", variable=varTicketType, value=1)
+radioTicketRework.grid(column=2, row=8, sticky="w")
+
+radioTicketDebug = Radiobutton(window, text="Debug", variable=varTicketType, value=2)
+radioTicketDebug.grid(column=2, row=9, sticky="w")
+
+radioTicketInv = Radiobutton(window, text="Inventory", variable=varTicketType, value=3)
+radioTicketInv.grid(column=2, row=10, sticky="w")
+
+radioTicketShipping = Radiobutton(window, text="Shipping", variable=varTicketType, value=4)
+radioTicketShipping.grid(column=2, row=11, sticky="w")
 
 # generate button
 generate = Button(window, text="Generate comment", width=80, command=createComment)
-generate.grid(columnspan=2, column=0, row=10)
+generate.grid(columnspan=2, column=0, row=12)
 
 # Copy to clipboard button
 copyButton = Button(window, text="Copy comment to clipboard", width=40, command=copyToClipboard)
-copyButton.grid(columnspan=1, column=2, row=10)
+copyButton.grid(columnspan=1, column=2, row=12)
 
 # text generation field
 generatedText = Text(window, width=120, height=10)
-generatedText.grid(columnspan=3, column=0, row=11)
-
+generatedText.grid(columnspan=3, column=0, row=13)
 
 window.mainloop()
